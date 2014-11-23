481a5f2bdb3007f2dd5530ec4cde83272db62b2a
Tomas Neme <lacrymology@gmail.com>
2014-11-23 14:06:31 +0800

    base python package

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 django-google-tag-manager
 =========================
 
-Template tag to install your Google Tag Manager account in your templates http://www.google.com/tagmanager/
+Template tag to install your Google Tag Manager account in your
+templates (http://www.google.com/tagmanager/)
+
+## Installation and Usage
+
+1. run `pip install django-google-tag-manager`
+2. add `'gtm'` to your `INSTALLED_APPS` setting.
+3. set `GOOGLE_TAG_ID` to your Google Tag Container Id. It
+   should look something like `GTM_XXXXXX`
+4. In your templates (probably in your base template) you `{% include
+   gtm_tags %}` and then `{% gtm %}` just below your `<body>` opening tag.
+5. Profit
+
+That's it for the most part. If for any reason you want to override
+the template used to render the tag, it's called `gtm/gtm.html`.
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+from gtm import __version__
+
+setup(
+    name = "django-google-tag-manager",
+    version = __version__,
+    author = "Tomas Neme",
+    author_email = "lacrymology@gmail.com",
+    description = ("Easily include your Google Tag Manager tag in your "
+                   "django site"),
+    license = "MIT",
+    keywords = "django generic-views",
+    url = "https://github.com/Lacrymology/django-google-tag-manager",
+    packages=['gtm'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
+
