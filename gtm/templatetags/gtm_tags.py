c475265f8c40a68ce757137e358d9446eeb570cf
Oleg Serov <oleg@serov.name>
2017-05-24 18:02:39 -0400

    Added two tags gtm_head and gtm_body to follow google instructions

--- gtm/templatetags/gtm_tags.py
+++ gtm/templatetags/gtm_tags.py
@@ -10,3 +10,19 @@ def gtm(google_tag_id=None):
     return {
         'google_tag_id': google_tag_id
     }
+
+@register.inclusion_tag("gtm/gtm_body.html")
+def gtm_head(google_tag_id=None):
+    if google_tag_id is None:
+        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None);
+    return {
+        'google_tag_id': google_tag_id
+    }
+
+@register.inclusion_tag("gtm/gtm_head.html")
+def gtm_body(google_tag_id=None):
+    if google_tag_id is None:
+        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None);
+    return {
+        'google_tag_id': google_tag_id
+    }
