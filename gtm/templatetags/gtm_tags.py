8eb6c0a11de8e3b9dcc7cca052a27b11bec7ce78
Max Peterson <max@incuna.com>
2017-11-04 20:35:29 +0000

    DRY templatetags.

--- gtm/templatetags/gtm_tags.py
+++ gtm/templatetags/gtm_tags.py
@@ -1,28 +1,17 @@
-from django.template import Library
 from django.conf import settings
+from django.template import Library
 
 register = Library()
 
-@register.inclusion_tag("gtm/gtm.html")
-def gtm(google_tag_id=None):
+
+def gtm_tag(google_tag_id=None):
     if google_tag_id is None:
-        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None);
+        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None)
     return {
         'google_tag_id': google_tag_id
     }
 
-@register.inclusion_tag("gtm/gtm_head.html")
-def gtm_head(google_tag_id=None):
-    if google_tag_id is None:
-        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None);
-    return {
-        'google_tag_id': google_tag_id
-    }
 
-@register.inclusion_tag("gtm/gtm_body.html")
-def gtm_body(google_tag_id=None):
-    if google_tag_id is None:
-        google_tag_id = getattr(settings, 'GOOGLE_TAG_ID', None);
-    return {
-        'google_tag_id': google_tag_id
-    }
+register.inclusion_tag("gtm/gtm.html", name='gtm')(gtm_tag)
+register.inclusion_tag("gtm/gtm_head.html", name='gtm_head')(gtm_tag)
+register.inclusion_tag("gtm/gtm_body.html", name='gtm_body')(gtm_tag)
